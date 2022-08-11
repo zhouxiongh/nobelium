0d52d5624c2f56f8fe60c9990cac7383e1bd7b92
hezhoux <hezhoux@qq.com>
2022-08-11 22:56:17 +0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -8,7 +8,7 @@ const NavBar = () => {
   const links = [
     { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
     { id: 1, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
-    { id: 2, name: locale.NAV.RSS, to: '/feed', show: true },
+    // { id: 2, name: locale.NAV.RSS, to: '/feed', show: true },
     { id: 3, name: locale.NAV.SEARCH, to: '/search', show: true },
     { id: 4, name: locale.NAV.EXPERIMENT, to: '/experiment', show: true },
     { id: 5, name: locale.NAV.PLAYGROUND, to: '/playground', show: true }
